a18bcf2e6f764c18d16a72c69b12663daae31869
ShivaniVora <shivanivora797@icloud.com>
2022-12-01 09:30:53 -0500

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from 'next/router';
+import PostPage from "../../components/postPage";
 import { useState } from "react";
 
 const Post = (props) => {
