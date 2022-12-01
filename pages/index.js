d769d25c08096ad05f64b11cd070c09a64faebba
Connor Duffy <connorpduffy160182@gmail.com>
2022-12-01 13:34:14 -0500

    Added pagination and resolved posts pages server side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
 import styles from "../styles/Home.module.css";
 import PostCreator from "../components/postCreator";
 import Link from "next/link";
-import CommentCreator from "../components/commentCreator";
+import { useState, useEffect } from 'react';
+
+const pageLimit = 10;
 
 export default function Home(props) {
 
-  const { allPosts } = props;
+  const [startFeed, setStartFeed] = useState(0);
+  const [feed, setFeed] = useState([]);
+  const [prompt, setPrompt] = useState("");
+
+  useEffect(() => {
+    console.log(startFeed);
+    let params = { skip:startFeed, limit:pageLimit }
+    fetch("/api/posts/feed", {
+      method: "POST",
+      body: JSON.stringify(params),
+    })
+      .then((res) => res.json())
+      .then((data) => setFeed(data));
+  },[startFeed])
 
   return (
     <div className={styles.row}>
@@ -15,7 +30,7 @@ export default function Home(props) {
       <div className={styles.main}>
         <h1>Feed</h1>
         {
-          allPosts.map((post)=> (
+          feed.map((post)=> (
             <div key={post["_id"]}>
               <Link href={"/posts/" + post["_id"]}>
                 <h3>{post.title}</h3>
@@ -23,13 +38,36 @@ export default function Home(props) {
               <p>{post.body}</p>
             </div>
         ))}
-      </div>
+        <div className={styles.row}>
+          <button onClick={() => {
+            if (startFeed - pageLimit < 0) {
+              setPrompt("There are no earlier posts.");
+            } else {
+              setPrompt("");
+              setStartFeed(startFeed - pageLimit);
+            }
+          }}>{"<"}</button>
+          <button onClick={() => {
+            setPrompt("");
+            setStartFeed(startFeed + pageLimit);
+          }}>{">"}</button>
+        </div>
+        <div>{prompt}</div>
+        <div><h3>Explore more Posts!</h3></div>
+        <div><h3>{startFeed}</h3></div>
+        </div>
+      
     </div>
   )
 }
 
 export async function getServerSideProps() {
-  const res = await fetch("http://localhost:3000/api/posts/feed");
+  let params = { skip:0, limit:pageLimit }
+  let res = await fetch("http://localhost:3000/api/posts/feed", {
+      method: "POST",
+      body: JSON.stringify(params),
+  });
+  // const res = await fetch("http://localhost:3000/api/posts/feed");
   const data = await res.json();
   return {
     props: {
